ef1421515d24b4aae136aa8c748c1edc5718d743
Arjun <arjun@paladinsoftwares.com>
2020-10-22 10:40:55 +0530

    media up

--- src/component/MediaUploads/MediaUploadsMaster.jsx
+++ src/component/MediaUploads/MediaUploadsMaster.jsx
@@ -1,73 +1,74 @@
 import React, { Component } from "react";
+import plus from "../../images/plus.png";
+import Grid from "@material-ui/core/Grid";
 import "./MediaUploadsMaster.css";
-import { Select } from "antd";
-import "antd/dist/antd.css";
-import Moment from "react-moment";
-import MediaUploadsTable from "./MediaUploadsTable";
-import MediaUploadsModal from "./MediaUploadsModal";
 import Modalcomp from "../../helper/ModalComp/ModalComp";
-import { Input } from "antd";
+import MediaUploadsModal from "./MediaUploadsModal";
+import MediaUploadsTable from "./MediaUploadsTable";
 import Button from '@material-ui/core/Button';
-import Plus from '../../images/plus.png'
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { Input, Select, Icon } from 'antd';
 import dateFormat from 'dateformat';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import Labelbox from "../../helper/labelbox/labelbox";
 import Paper from '@material-ui/core/Paper';
 
-const current_date=(dateFormat(new Date(),"dd mmm yyyy"))
+const current_date = (dateFormat(new Date(), "dd mmm yyyy"))
 
-export default  class MediaUploadsMaster extends Component {
+
+export default class MediaUploadsMaster extends Component {
   constructor(props) {
     super(props);
     this.state = {
       open: false
     };
   }
+
   handleClickopen = () => {
     this.setState({ open: true });
   };
+
   handleClickclose = () => {
     this.setState({ open: false });
   };
 
+  
   render() {
     const { Search } = Input;
-    console.log(dateFormat(new Date(),"dd mmm yyyy"))
+    console.log(dateFormat(new Date(), "dd mmm yyyy"))
     return (
-      <div className="uploadmaster_doctor">
-        <div className="media_uploadsmasterheader">
-              <div className="titleuser_media_diet">MEDIA UPLOADS</div>
-           
-           
-                <img
-                  clasName="plus-icon-media"
-                  src={Plus}
-                  alt={"hi"}
-                  onClick={this.handleClickopen}
-                />
-                
-              </div>
-        
-      
-        <MediaUploadsTable />
-        <div className="Upload-modal-container">
-          <Modalcomp
-           clrchange="text_color"
-            visible={this.state.open}
-            closemodal={this.handleClickclose}
-            title={"NEW MEDIA UPLOADS"}
-            custommodalsize="media_newmedia"
-         
-          >
-            <MediaUploadsModal 
-            custommodalsize="media_uploadmodal"
-            visible={this.state.open}
-            closemodal={this.handleClickclose}
-          />
-          </Modalcomp>
-        </div>
+      <div className="">
+        <Paper>
+          <div className="dashboard_header">
+            <div className="titleuser">MEDIA UPLOADS</div>
+
+            <div className="doctor_plus-container">
+
+              <img
+                className="plus-icon"
+                src={plus}
+                alt={"hi"}
+                onClick={this.handleClickopen}
+              />
+
+            </div>
+          </div>
+
+          <MediaUploadsTable  truegetmethod={this.state.truegetmethod} falsegetmethod={()=>this.setState({truegetmethod:false})}   generateAlert={this.props.generateAlert} userId={this.props.userId} />
+          <div className="Upload-modal-container">
+            <Modalcomp
+              visible={this.state.open}
+              closemodal={this.handleClickclose}
+              title={"New Media Uploads"}
+            >
+              <MediaUploadsModal visible={this.state.open}
+                closemodal={this.handleClickclose}
+                userId={this.props.userId}
+              
+            getTableData={()=>this.setState({truegetmethod:true})} 
+            generateAlert={this.props.generateAlert}
+              />
+            </Modalcomp>
+          </div>
+        </Paper>
       </div>
     );
   }
--- src/component/MediaUploads/MediaUploadsTable.jsx
+++ src/component/MediaUploads/MediaUploadsTable.jsx
@@ -1,123 +1,166 @@
 import React from "react";
-import Tablecomponent from "../../helper/TableComponent/TableComp";
+// import Tablecomponent from "../../helpers/TableComponent/TableComp";
 import Modalcomp from "../../helper/ModalComp/ModalComp";
-import Card from "@material-ui/core/Card";
-import Button from "@material-ui/core/Button";
-import { NavLink } from "react-router-dom";
-import "./MediaUploadsTable.css";
-import crowngold from "../../images/crown-golden.png";
-import dateFormat from "dateformat";
-import Greenwalk from "../../images/greenwalk.png";
-import PhoneIphoneIcon from "@material-ui/icons/PhoneIphone";
-import MediaUploadsModal from "./MediaUploadsModal";
-import UploadMedia from "./UploadMedia";
-import Ordericon from "../../images/order.svg";
 import ViewMedia from "./ViewMedia";
-const current_date = dateFormat(new Date(), "dd mmm yyyy");
+import MediaUploadsModal from "./MediaUploadsModal";
+import order from "../../images/order.svg";
+import "./MediaUploadsTable.css";
+import ReactSVG from "react-svg";
+import axios from 'axios';
+import { apiurl } from "../../App";
+import dateformat from 'dateformat';
+import DeleteMedia from '../../helper/ModalComp/deleteModal';
+import DragdropTable from "./DragTable.js"
 
-class DashboardTable extends React.Component {
+class MediaUploadsTable extends React.Component {
   state = {
     openview: false,
-    workflow: false,
+    deleteopen: false,
+    tableData:[],
+    editData: "",
+    viewData:"",
+    // workflow:false,
   };
+  componentDidMount() {
+    this.getTableData()
+}
 
-  createData = (parameter) => {
-    var keys = Object.keys(parameter);
-    var values = Object.values(parameter);
+componentWillReceiveProps(newProps) {
+  if(newProps.truegetmethod){
+    this.getTableData()
+    this.props.falsegetmethod()
+  }
+}
 
-    var returnobj = {};
-
-    for (var i = 0; i < keys.length; i++) {
-      returnobj[keys[i]] = values[i];
-    }
-    return returnobj;
-  };
-
-  modelopen = (data) => {
+  modelopen = (data,id) => {
+    console.log(data,"data_checking")
     if (data === "view") {
-      this.setState({ workflow: true });
-    } else if (data === "edit") {
-      this.setState({ editopen: true });
+        console.log(data,"edit_data")
+        this.setState({ openview:true })
+        this.setState({
+          viewData:this.state.totalData[0].details.find(val =>val.id ===id)
+        })
+        console.log(this.state.viewData,"view_id_check")
+        // console.log(id,"id_view")
     }
-  };
+    else if (data === "edit") {
+        this.setState({ editopen: true })
+        this.setState({
+            editData:this.state.totalData[0].details.find(val => val.id ===id)
+        })
+        // console.log(this.state.totalData,"dataaa")
+        console.log(this.state.editData,"editdata_checkk")
+        // alert(this.state.editData,"editdata_check_table")
+        // console.log(id,"id_check")
+        // Console.log(editData,"editdata_table_check")
+        // console.log(this.state.totalData.details,"total_data_check")
+    }
+}
+   // get table data
+   getTableData = () => {
+    var self = this
+    axios({
+        method: 'POST', //get method 
+        url: apiurl + 'Common/mediaupload_details',
+        data:{
+          // doctorid: this.props.userId,
+          vendor_id:0,
+          limit:100,
+          offset:1,
+          pageno:1         
+        }
+    })
+    .then((response) => {
+      // console.log(response.data.data[0].details,"res")
+      console.log(response,"response_data_table")
+      var tableData = [];
+        response.data.data[0].details.map((val,index) => {
+          console.log("sdfjsdhfjdshf",val)
+          // for(let i=0;i<50;i++){
+            tableData.push({ title: val.media_title,type:val.media_type,uploaded:dateformat(val.created_on,"dd mmm yyyy hh:MM TT"),status:val.is_active,id: val.id,indexid:index.toString(),sortorder:val.media_sortorder })
+            console.log(val.id,"idddddd")
+          // }
 
-  closemodal = () => {
-    this.setState({ openview: false, editopen: false, workflow: false });
-  };
-
+        })
+        self.setState({
+            tableData:tableData,
+            props_loading: false,
+            totalData:response.data.data
+        })
+        // console.log(tableData,"table")
+        console.log(response.data.data[0].details,"iddd_checkkk")
+    }).catch((error) => {
+    })
+}
+closemodal = () => {
+  this.setState({ openview:false,editopen:false, deleteopen:false,workflow:false})
+}
+deleteopen = (type,id) => {
+  this.setState({
+      deleteopen: true,
+      iddata: id
+  })
+  console.log(id,"type")
+}
+deleterow = () => {
+  this.setState({ props_loading: true })
+  var self = this
+  axios({
+      method: 'delete',
+      url: apiurl + '/deleteMediaUpload',
+      data: {
+          "id": this.state.iddata,
+      }
+  })
+      .then(function (response) {
+          self.getTableData()
+          self.props.generateAlert("Record Deleted Successfully")
+      })
+      .catch(function (error) {
+      });
+  this.setState({ props_loading: false })
+}
   render() {
+    console.log("sadfkjsdhfljhsdfkj",this.state.tableData)
+    const img_var = <ReactSVG src={order} />;
     return (
       <div>
-        <Tablecomponent
-          heading={[
-            { id: "order", label: "Order" },
-            { id: "", label: "S.No" },
-            { id: "media_title", label: "Media Title" },
-            { id: "media_type", label: "Media Type" },
-            { id: "uploaded_on", label: "Uploaded On" },
-            { id: "status", label: "Status" },
-            { id: "", label: "Action" },
-          ]}
-          rowdata={[
-            this.createData({
-              order: <img src={Ordericon} />,
-              media_title: "Chiken Dinner",
-              media_type: "Video",
-              uploaded_on: "24 Aug 2019 10:00",
-              status: "Active",
-            }),
-            this.createData({
-              order: <img src={Ordericon} />,
-              media_title: "Egg,Fries,Toast",
-              media_type: "Image",
-              uploaded_on: "2 Aug 2019 09:00",
-              status: "Active",
-            }),
-            this.createData({
-              order: <img src={Ordericon} />,
-              media_title: "Chiken Dinner",
-              media_type: "Video",
-              uploaded_on: "20 Aug 2019 01:00",
-              status: "Active",
-            }),
-            this.createData({
-              order: <img src={Ordericon} />,
-              media_title: "Vegetable Dinner",
-              media_type: "Image",
-              uploaded_on: "4 Aug 2019 10:10",
-              status: "Inactive",
-            }),
-          ]}
-          tableicon_align={"cell_eye"}
-          modelopen={(e) => this.modelopen(e)}
-          Workflow="close"
-          add="close"
+      
+        <DragdropTable 
+                  heading={[
+                     {id:"order", label:"Order"},
+                    { id: "", label: "S.No" },
+                    { id: "title", label: "Media Title" },
+                    { id: "type", label: "Media Type" },
+                    { id: "uploaded", label: "Uploaded On" },
+                    { id: "status", label: "Status" },
+                    { id: "", label: "Action" },
+                  ]}
+          rowdata={this.state.tableData && this.state.tableData}
+          deleteopen={this.deleteopen}
+          props_loading={this.state.props_loading}
+          modelopen={(e,id) => this.modelopen(e,id)}
 
         />
-
+        {/* <ViewMedia open={this.state.openview} onClose={this.closemodal} /> */}
         <Modalcomp
-          clrchange="text_color"
-          custommodalsize="media_viewmedia"
-          visible={this.state.workflow}
+          visible={this.state.openview}
           title={"VIEW MEDIA"}
           closemodal={(e) => this.closemodal(e)}
-
+          xswidth={"xs"}
         >
-          <ViewMedia />
+          <ViewMedia visible={this.state.openview} viewData={this.state.viewData} viewopenModal ={this.state.openview && true}/>
         </Modalcomp>
 
-        <Modalcomp
-          clrchange="text_color"
-          custommodalsize="media_newmedia"
-          visible={this.state.editopen}
-          title={"EDIT MEDIA UPLOADS"}
-          closemodal={(e) => this.closemodal(e)}
-        >
-          <MediaUploadsModal />
+        <Modalcomp  visible={this.state.editopen} editData={this.state.editData}  title={"EDIT MEDIA UPLOADS"} closemodal={(e) => this.closemodal(e)} >
+          <MediaUploadsModal getTableData={this.getTableData} closemodal ={this.closemodal} editData={this.state.editData} editopenModal ={this.state.editopen && true}  generateAlert={this.props.generateAlert} userId={this.props.userId} />
         </Modalcomp>
+
+        <Modalcomp  visible={this.state.deleteopen} title={"Delete"} closemodal={this.closemodal} xswidth={"xs"}>
+           <DeleteMedia deleterow={this.deleterow} closemodal={this.closemodal}     generateAlert={this.props.generateAlert}  />
+         </Modalcomp>
       </div>
     );
   }
 }
-
-export default DashboardTable;
+export default MediaUploadsTable
